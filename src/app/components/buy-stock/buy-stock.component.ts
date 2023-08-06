25b574e21efd80d10ffd2433d4bba9ca7a3802b9
Tobias Münch <tobias.muench@prodyna.com>
2023-08-06 20:04:17 +0200

    make all features reactive

--- src/app/components/buy-stock/buy-stock.component.ts
+++ src/app/components/buy-stock/buy-stock.component.ts
@@ -1,95 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StockSymbolAndAmountFormValue } from '../stock-symbol-and-amount-input/stock-symbol-and-amount-input.component';
 import {
   StockPriceData,
   StockPriceDataService,
 } from 'src/app/services/stock-price-data.service';
 import { AccountService } from 'src/app/services/account.service';
+import {
+  BehaviorSubject,
+  Observable,
+  ReplaySubject,
+  Subject,
+  catchError,
+  combineLatest,
+  finalize,
+  forkJoin,
+  map,
+  of,
+  share,
+  shareReplay,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 
 @Component({
   selector: 'app-buy-stock',
   templateUrl: './buy-stock.component.html',
   styleUrls: ['./buy-stock.component.scss'],
 })
-export class BuyStockComponent {
-  selectedStockAndSymbol: StockSymbolAndAmountFormValue | null = null;
+export class BuyStockComponent implements OnInit {
+  selectedStockAndAmount: StockSymbolAndAmountFormValue | null = null;
   stockPriceData: StockPriceData | null = null;
-  isPriceLoading: boolean = false;
   showAmountNotAvailableWarning: boolean = false;
+
   isAmountAvailbilityLoading: boolean = false;
+  isPriceLoading: boolean = false;
+
+  readonly stockAndAmountSelectionSubject =
+    new ReplaySubject<StockSymbolAndAmountFormValue | null>(1);
+
+  readonly reloadSubject = new BehaviorSubject<void>(undefined);
 
   constructor(
     private readonly stockPriceDataService: StockPriceDataService,
     private readonly accountService: AccountService
   ) {}
 
-  stockAndAmountSelectionChanged(
-    selection: StockSymbolAndAmountFormValue | null
-  ) {
-    this.selectedStockAndSymbol = selection;
-    if (selection) {
-      this.loadStockPrice(selection);
-    } else {
-      this.stockPriceData = null;
-    }
+  stockPriceData$: Observable<StockPriceData | null> = combineLatest([
+    this.stockAndAmountSelectionSubject,
+    this.reloadSubject,
+  ]).pipe(
+    tap(() => {
+      this.showAmountNotAvailableWarning = false;
+    }),
+    switchMap(([selection, _]) =>
+      selection ? this.loadStockPrice(selection) : of(null)
+    ),
+    share()
+  );
+
+  showAmountNotAvailableWarning$: Observable<boolean> =
+    this.stockPriceData$.pipe(
+      switchMap(stockPriceData =>
+        stockPriceData
+          ? this.loadAmountAvailability(stockPriceData.price).pipe(
+              map(isAmountAvailable => !isAmountAvailable)
+            )
+          : of(false)
+      )
+    );
+
+  ngOnInit() {
+    this.stockAndAmountSelectionSubject.subscribe(selectedStockAndAmount => {
+      this.selectedStockAndAmount = selectedStockAndAmount;
+    });
+
+    this.stockPriceData$.subscribe(stockPriceData => {
+      this.stockPriceData = stockPriceData;
+    });
+
+    this.showAmountNotAvailableWarning$.subscribe(
+      showAmountNotAvailableWarning => {
+        this.showAmountNotAvailableWarning = showAmountNotAvailableWarning;
+      }
+    );
   }
 
-  onReload() {
-    this.loadStockPrice(this.selectedStockAndSymbol!);
-  }
-
-  private loadStockPrice(selection: StockSymbolAndAmountFormValue) {
-    this.isPriceLoading = true;
-    this.showAmountNotAvailableWarning = false;
-    this.parallelLoadPriceFromSIXAndXETRA(selection);
-  }
-
-  private parallelLoadPriceFromSIXAndXETRA(
+  private loadStockPrice(
     selection: StockSymbolAndAmountFormValue
-  ) {
-    let sixLoading = true;
-    let xetraLoading = true;
+  ): Observable<StockPriceData> {
+    this.isPriceLoading = true;
 
-    this.stockPriceDataService
-      .getPriceFromSIX(selection.symbolInput, selection.amountInput)
-      .subscribe(loadedStockPriceData => {
-        if (xetraLoading) {
-          this.stockPriceData = loadedStockPriceData;
-          sixLoading = false;
-        } else {
-          this.stockPriceData =
-            loadedStockPriceData.price < this.stockPriceData!.price
-              ? loadedStockPriceData
-              : this.stockPriceData;
-          this.isPriceLoading = false;
-          this.loadAmountAvailability(this.stockPriceData!.price);
-        }
-      });
-
-    this.stockPriceDataService
-      .getPriceFromXETRA(selection.symbolInput, selection.amountInput)
-      .subscribe(loadedStockPriceData => {
-        if (sixLoading) {
-          this.stockPriceData = loadedStockPriceData;
-          xetraLoading = false;
-        } else {
-          this.stockPriceData =
-            loadedStockPriceData.price < this.stockPriceData!.price
-              ? loadedStockPriceData
-              : this.stockPriceData;
-          this.isPriceLoading = false;
-          this.loadAmountAvailability(this.stockPriceData!.price);
-        }
-      });
+    return forkJoin([
+      this.stockPriceDataService.getPriceFromSIX(
+        selection.symbolInput,
+        selection.amountInput
+      ),
+      this.stockPriceDataService.getPriceFromXETRA(
+        selection.symbolInput,
+        selection.amountInput
+      ),
+    ]).pipe(
+      map(([sixPrice, xetraPrice]) =>
+        sixPrice.price < xetraPrice.price ? sixPrice : xetraPrice
+      ),
+      tap(() => {
+        this.isPriceLoading = false;
+      })
+    );
   }
 
-  private loadAmountAvailability(amount: number) {
+  private loadAmountAvailability(amount: number): Observable<boolean> {
     this.isAmountAvailbilityLoading = true;
-    this.accountService
-      .isAmountAvailable(amount)
-      .subscribe(isAmountAvailable => {
-        this.showAmountNotAvailableWarning = !isAmountAvailable;
+    return this.accountService.isAmountAvailable(amount).pipe(
+      tap(() => {
         this.isAmountAvailbilityLoading = false;
-      });
+      })
+    );
   }
 }
