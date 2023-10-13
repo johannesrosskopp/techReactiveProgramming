38201903e16c0576639fa0c2679f89cffb777d93
Johannes Rosskopp <johannes.rosskopp@prodyna.com>
2023-10-13 08:31:02 +0000

    hands-on-1

--- src/app/components/buy-stock/buy-stock.component.ts
+++ src/app/components/buy-stock/buy-stock.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StockSymbolAndAmountFormValue } from '../stock-symbol-and-amount-input/stock-symbol-and-amount-input.component';
+import { ReplaySubject, Subject, Subscription, take } from 'rxjs';
+import { StockSymbol } from 'src/app/constants/availableStocks';
 
 @Component({
   selector: 'app-buy-stock',
@@ -10,7 +12,8 @@ export class BuyStockComponent implements OnInit {
   selectedStockAndAmount: StockSymbolAndAmountFormValue | null = null;
 
   // TODO create a subject that will hold the selected stock and amount
-  // private readonly stockAndAmountSelectionSubject =
+  private readonly stockAndAmount$ =
+    new ReplaySubject<StockSymbolAndAmountFormValue>(1);
 
   constructor() {}
 
@@ -18,18 +21,27 @@ export class BuyStockComponent implements OnInit {
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {
     // TODO subscribe to the subject and update the 'selectedStockAndAmount' variable
+    this.stockAndAmount$.subscribe(selectedStockAndAmount => {
+      this.selectedStockAndAmount = selectedStockAndAmount;
+    });
   }
 
   onStockAndAmountSelectionChange(
     selection: StockSymbolAndAmountFormValue | null
   ) {
-    // TODO insert the selection into the subject
+    if (selection) {
+      this.stockAndAmount$.next(selection);
+    }
   }
 
   onSearchStockBtnClicked() {
     // TODO subscribe to the subject limited to one emission (take(1)) and use following code to open a new tab with the google search
-    // window.open(
-    //   `https://www.google.com/search?q=${selectedStockAndAmount?.symbolInput}`
-    // );
+    //this.stockAndAmount$.pipe(take(1)).subscribe(selectedStockAndAmount => {
+    //  this.selectedStockAndAmount = selectedStockAndAmount;
+    //});
+
+    window.open(
+      `https://www.google.com/search?q=${this.selectedStockAndAmount?.symbolInput}`
+    );
   }
 }
